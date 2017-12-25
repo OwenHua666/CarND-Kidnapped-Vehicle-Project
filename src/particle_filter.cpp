75a7a5d6b36c2c539e8a3c84c801b148260655fa
Yiwen <owen.hua930531@hotmail.com>
2017-12-25 11:45:54 -0500

    Error reduced and passed the simulator test.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 
-    num_particles = 10;
+    num_particles = 105;
 
     // Set the size of the weight
     weights.resize(num_particles);
